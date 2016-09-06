ec4d6d8cd257f4ba2998cb115eb2291d54eb4a42
Komari Herring <komherr@gmail.com>
2016-09-06 01:56:17 -0400

    Seconds

--- Seconds/main.m
+++ Seconds/main.m
@@ -2,15 +2,34 @@
 //  main.m
 //  Seconds
 //
-//  Created by Komari Herring on 9/6/16.
+//  Created by Komari Herring on 9/4/16.
 //  Copyright © 2016 The Iron Yard. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+
+int main(int argc, const char * argv[])
+
+
+{
+    @autoreleasepool
+    
+    {
+        //common knowledge 
+        int secsInMin = 60;
+        int minInHour = 60;
+        int hoursInDay = 24;
+        int daysInYear = 365;
+        
+        
+        int secondsInYear =secsInMin * minInHour * hoursInDay * daysInYear;
+        
+        NSLog(@"%d seconds in a year", secondsInYear);
+        
+        
     }
+    
+    
+    return 0;
 }
